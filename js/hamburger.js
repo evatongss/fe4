c22b6de3d3db64ae2d8e176ca9947fd54806f5cf
Prince <evatong1023.com>
2025-07-31 17:23:32 +0800

    intial backup

--- js/hamburger.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const toggleMenu = document.querySelector(".navbar .mobile-menu-toggle");
-    const mobileMenu = document.querySelector(".navbar .mobile-menu-items");
-
-    toggleMenu.addEventListener("click", () => {
-        mobileMenu.classList.toggle("active");
-    });
-});
--- js/toggle.js
+++
@@ -1,7 +0,0 @@
-const button = document.querySelector('#btn-1');
-const heading = document.querySelector('h1');
-
-button.addEventListener('click', () => {
-  document.body.classList.toggle("dark");
-  heading.classList.toggle("yellow");
-});
